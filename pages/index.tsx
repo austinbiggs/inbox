ae6a78367e7f8783219f503da21c421ba44d7d0c
Austin Biggs <austintbiggs@gmail.com>
2021-11-29 13:21:01 -0700

    Change project name

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
       <DocHead />
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>Inbox</h1>
+          <h1 className={styles.title}>Messenger</h1>
 
           <Inbox />
         </main>
