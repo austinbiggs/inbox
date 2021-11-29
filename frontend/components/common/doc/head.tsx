31835f2f2178121c50d90d031a4e4c3244e6bfd3
Austin Biggs <austintbiggs@gmail.com>
2021-11-29 12:14:20 -0700

    Update title + head; add favicon + logo

--- frontend/components/common/doc/head.tsx
+++ frontend/components/common/doc/head.tsx
@@ -4,9 +4,30 @@ import Head from "next/head";
 const DocHead: React.FC = () => {
   return (
     <Head>
-      <title>Inbox by Austin Biggs</title>
-      <meta name="description" content="Inbox by Austin Biggs" />
-      <link rel="icon" href="/favicon.ico" />
+      <title>Handshake Messenger</title>
+      <meta name="description" content="Handshake Messenger by Austin Biggs" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
