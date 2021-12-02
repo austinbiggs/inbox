92a6e32bb56d26960755e8f9c4df01bb9b64137b
Austin Biggs <austintbiggs@gmail.com>
2021-12-02 10:45:14 -0700

    Adjust the env.local example; Modify the errorlink to be less annoying

--- frontend/gql/client/link.js
+++ frontend/gql/client/link.js
@@ -1,48 +1,60 @@
 import { from, HttpLink, split } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
-import { WebSocketLink } from '@apollo/client/link/ws';
+import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { SubscriptionClient } from "subscriptions-transport-ws";
 import Bugsnag from "@bugsnag/js";
 
 const hasuraEndpoint = process.env.NEXT_PUBLIC_HASURA_ENDPOINT;
-const hasuraWebSockerEndpoint = process.env.NEXT_PUBLIC_HASURA_WEBSOCKET_ENDPOINT;
+const hasuraWebSocketEndpoint =
+  process.env.NEXT_PUBLIC_HASURA_WEBSOCKET_ENDPOINT;
 const hasuraAdminSecret = process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET;
 
 const headers = {
   "x-hasura-admin-secret": hasuraAdminSecret,
 };
 
-const errorLink = onError(({ networkError }) => Bugsnag.notify(networkError));
+const errorLink = onError(({ networkError }) => {
+  if (networkError) {
+    if (
+      process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "test"
+    ) {
+      console.log("Apollo encountered a network error", { networkError });
+    } else {
+      Bugsnag.notify(networkError);
+    }
+  }
+});
 
 const httpLink = new HttpLink({
   headers,
   uri: hasuraEndpoint,
 });
 
-const isBrowser = typeof window !== 'undefined'
+const isBrowser = typeof window !== "undefined";
 
 // WS doesn't work on the server
 const wsLink = isBrowser
   ? new WebSocketLink(
-    new SubscriptionClient(hasuraWebSockerEndpoint, {
-      reconnect: true,
-      timeout: 30000,
-      connectionParams: {
-        headers,
-      }
-    })
-  )
+      new SubscriptionClient(hasuraWebSocketEndpoint, {
+        reconnect: true,
+        timeout: 30000,
+        connectionParams: {
+          headers,
+        },
+      })
+    )
   : null;
 
 export const link = isBrowser
   ? split(
-    ({ query }) => {
-      const { kind, operation } = getMainDefinition(query)
-      console.log({query}, {kind}, {operation});
-      return kind === 'OperationDefinition' && operation === 'subscription'
-    },
-    wsLink,
-    httpLink,
-  )
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        console.log({ query }, { kind }, { operation });
+        return kind === "OperationDefinition" && operation === "subscription";
+      },
+      wsLink,
+      httpLink
+    )
   : from([errorLink, httpLink]);
