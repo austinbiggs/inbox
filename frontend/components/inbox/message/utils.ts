88b620fdca8e8de51cf42084b1e521278bfa5db7
Austin Biggs <austintbiggs@gmail.com>
2021-12-02 12:29:00 -0700

    Create relative time helper; switch timestamps to h6

--- frontend/components/inbox/message/utils.ts
+++ frontend/components/inbox/message/utils.ts
@@ -1,5 +1,9 @@
-import { format, parseISO } from "date-fns";
+import { format, formatDistance, parseISO } from "date-fns";
 
 export function formattedTimeFromTimestamp(timestamp: string): string {
-  return format(parseISO(timestamp), 'hh:mm a')
+  return format(parseISO(timestamp), "hh:mm a");
+}
+
+export function relativeTimefromTimestamp(timestamp: string): string {
+  return formatDistance(parseISO(timestamp), new Date(), { addSuffix: true });
 }
