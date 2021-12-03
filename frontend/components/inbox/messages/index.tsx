f71fd5b75478b4982b6ad9ad81632bae05d4bbd2
Adam Hill <ahill@joinhandshake.com>
2021-12-03 12:12:42 -0500

    Add relative date headers

--- frontend/components/inbox/messages/index.tsx
+++ frontend/components/inbox/messages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Message } from '../message';
+import { formattedRelativeTimeFromTimestamp } from '../message/utils';
+import styles from './styles.module.scss';
 import { Message as MessageType } from './types';
 
 interface Props {
@@ -19,16 +21,29 @@ const Messages = ({ messages }: Props): JSX.Element => {
     scrollToBottomOfMessages()
   }, [messagesAsString])
 
+  const relativeDateMap = new Map<string, string>()
+
+  messages.forEach(message => {
+    const relativeDate = formattedRelativeTimeFromTimestamp(message.timestamp)
+    if (!Array.from(relativeDateMap.values()).includes(relativeDate)) {
+      relativeDateMap.set(message.id, relativeDate)
+    }
+  })
+
   return (
     <>
       {messages.map(message => (
-        <Message 
-          id={message.id}
-          message={message.message}
-          timestamp={message.timestamp}
-          user={message.user}
-          key={`message-${message.id}`}
-        />
+        <div key={`message-${message.id}`}>
+          {relativeDateMap.has(message.id) && (
+            <p className={styles['relative-date-header']}>{relativeDateMap.get(message.id)}</p>
+          )}
+          <Message
+            id={message.id}
+            message={message.message}
+            timestamp={message.timestamp}
+            user={message.user}
+          />
+        </div>
       ))}
       <div ref={messagesEndRef} />
     </>
